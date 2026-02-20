c85c622a92d18051e8c828a53a7a4ca43e453f3b
Carlos Sierra <carlos.sierra@nav.no>
2026-02-20 13:21:35 +0100

    Adding bean execution and test messages

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/bean/PgBean.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/bean/PgBean.java
@@ -2,12 +2,21 @@ package no.nav.sikkerhetstjenesten.loggkamel.bean;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class PgBean {
+    private static final Logger log = LoggerFactory.getLogger(PgBean.class);
+
     public void extract(Exchange exchange) {
         Message msg = exchange.getMessage();
         String body = msg.getBody(String.class);
+        //log.error("Message body: " + body);
+        exchange.setVariable("test variable", UUID.randomUUID().toString());
+        log.error("Exchange variables at the end of bean execution: " + exchange.getVariables());
     }
 }
--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/route/PgRoute.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/route/PgRoute.java
@@ -1,5 +1,6 @@
 package no.nav.sikkerhetstjenesten.loggkamel.route;
 
+import no.nav.sikkerhetstjenesten.loggkamel.bean.PgBean;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,10 @@ public class PgRoute extends RouteBuilder {
                 //.setProperty("navIdent", body())    //simple("${body} regex '[A-Z]\\d{6}'"))
                 //.log("Nav-ident: ${exchangeProperty.navIdent}")
                 .log("Message: ${body}")
+                .bean(PgBean.class, "extract")
+                // Once the PgBean.extract() method has been run, confirm that you have added fields to the exchange variables that
+                // can be read and logged
+                .log("Per-message variables visible in the route after bean execution: ${variables}")
                 .toD("file:src/main/resources/files/output/");
     }
 }
