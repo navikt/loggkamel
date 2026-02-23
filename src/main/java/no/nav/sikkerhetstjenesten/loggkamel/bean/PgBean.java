ab520d1a10625908303eb5eb88d55fe8e61425b8
Carlos Sierra <carlos.sierra@nav.no>
2026-02-23 14:18:20 +0100

    Adding token-based parsing of log message for necessary values

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/bean/PgBean.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/bean/PgBean.java
@@ -6,7 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class PgBean {
@@ -15,8 +16,32 @@ public class PgBean {
     public void extract(Exchange exchange) {
         Message msg = exchange.getMessage();
         String body = msg.getBody(String.class);
-        //log.error("Message body: " + body);
-        exchange.setVariable("test variable", UUID.randomUUID().toString());
-        log.error("Exchange variables at the end of bean execution: " + exchange.getVariables());
+
+        String regex = "^(.*)\\(\\d+\\):v-oidc-(.*)-\\d+-.*@(.*?):.*(READ|WRITE|FUNCTION|DDL|MISC|MISC_SET),.*?,.*?,.*?,\"([\\s\\S]*)\",(.*)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(body);
+
+        // Check if the input string matches the template pattern
+        if (matcher.find()) {
+//            log.info("time: " + matcher.group(1));
+//            log.info("navIdent: " + matcher.group(2));
+//            log.info("dbName: " + matcher.group(3));
+//            log.info("pgAudit Log Type: " + matcher.group(4));
+//            log.info("SQL command: " + matcher.group(5));
+//            log.info("rest: " + matcher.group(6));
+
+            exchange.setVariable("logTime", matcher.group(1));
+            exchange.setVariable("navIdent", matcher.group(2));
+            exchange.setVariable("dbName", matcher.group(3));
+            exchange.setVariable("logType", matcher.group(4));
+            exchange.setVariable("sqlCommand", matcher.group(5));
+
+            msg.setBody(body);
+        } else {
+            log.error("Log failed to match expected pattern");
+//            throw new RuntimeException("Log failed to match expected pattern");
+        }
+
     }
 }
--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/route/PgRoute.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/route/PgRoute.java
@@ -12,6 +12,9 @@ public class PgRoute extends RouteBuilder {
 
     @Override
     public void configure() {
+        onException(RuntimeException.class)
+                .to("file:src/main/resources/files/output/dead-letter?fileExist=Append");
+
         //from("quartz://myGroup/myTestTimer?cron=*/10+*+*+*+*+?")
         //from("timer://myTimer?period=600000")
         from("file:src/main/resources/files?noop=true")
@@ -21,9 +24,14 @@ public class PgRoute extends RouteBuilder {
                 //.log("Nav-ident: ${exchangeProperty.navIdent}")
                 .log("Message: ${body}")
                 .bean(PgBean.class, "extract")
+                // TODO: Exception handling sending the message to a dead letter queue or equivalent
                 // Once the PgBean.extract() method has been run, confirm that you have added fields to the exchange variables that
                 // can be read and logged
                 .log("Per-message variables visible in the route after bean execution: ${variables}")
-                .toD("file:src/main/resources/files/output/");
+                // TODO: Confirm that you can access individual variables by name here
+                // TODO: Have the output location depend on (incorporate) db name
+                .toD("file:src/main/resources/files/output/?fileExist=Append");
+
+
     }
 }
