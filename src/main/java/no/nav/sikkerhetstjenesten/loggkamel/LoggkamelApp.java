d53b2a2ab4d4afc3d6ded04b85ad3fa04bd7d204
Carlos Sierra <carlos.sierra@nav.no>
2026-03-05 11:54:01 +0100

    Troubleshooting local environment being assigned in dev

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/LoggkamelApp.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/LoggkamelApp.java
@@ -1,15 +1,22 @@
 package no.nav.sikkerhetstjenesten.loggkamel;
 
 import no.nav.boot.conditionals.Cluster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static no.nav.boot.conditionals.Cluster.NAIS_CLUSTER_NAME;
+
 @SpringBootApplication
 public class LoggkamelApp {
+    private static final Logger log = LoggerFactory.getLogger(LoggkamelApp.class);
 
     public static void main(String[] args) {
         var loggKamelApp = new SpringApplication(LoggkamelApp.class);
         loggKamelApp.setAdditionalProfiles(Cluster.profiler());
+        //TODO: remove after troubleshooting
+        log.error("System environment is: " + System.getenv(NAIS_CLUSTER_NAME));
         loggKamelApp.run(args);
     }
 }
