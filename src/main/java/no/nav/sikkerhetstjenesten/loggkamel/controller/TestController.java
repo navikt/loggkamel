4f3de5341c03592e63ea5411a9ec14d78956fa64
Carlos Sierra <carlos.sierra@nav.no>
2026-03-09 09:53:41 +0100

    Add logging to test controller

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/controller/TestController.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/controller/TestController.java
@@ -1,10 +1,11 @@
 package no.nav.sikkerhetstjenesten.loggkamel.controller;
 
-import no.nav.boot.conditionals.ConditionalOnDev;
 import no.nav.boot.conditionals.ConditionalOnDevOrLocal;
 import no.nav.security.token.support.spring.UnprotectedRestController;
 import no.nav.sikkerhetstjenesten.loggkamel.client.EntraProxyAnsatt;
 import no.nav.sikkerhetstjenesten.loggkamel.service.EntraProxyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 @UnprotectedRestController("/api/v1")
 @ConditionalOnDevOrLocal
 public class TestController {
+
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     private final EntraProxyService entraProxyService;
 
@@ -21,7 +24,8 @@ public class TestController {
     }
 
     @GetMapping("ansatt/{navIdent}")
-    public EntraProxyAnsatt getLoggkamel(@PathVariable("navIdent") String navIdent) {
+    public EntraProxyAnsatt getAnsattForNavident(@PathVariable("navIdent") String navIdent) {
+        log.info("Fetching Ansatt for navIdent: {}", navIdent);
         return entraProxyService.getAnsattFromNavIdent(navIdent);
     }
 }
