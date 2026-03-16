39116e8d5fbbd95567b331aa29032c31fb6621a7
Carlos Sierra <carlos.sierra@nav.no>
2026-03-16 13:13:49 +0100

    Set CamelFileName from google name when missing, make unmarshal conditional

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/routes/consumer/PostgresLogConsumer.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/routes/consumer/PostgresLogConsumer.java
@@ -32,13 +32,12 @@ public class PostgresLogConsumer extends RouteBuilder {
         // Alternatively, pull out of here and into a universal route definition
 
         //TODO: decide whether to re-compress failed messages, or leave uncompressed
-        // Delivery / technical failures for otherwise valid messages
         errorHandler(deadLetterChannel(deadLetterUri)
 //                .useOriginalMessage()
                 .maximumRedeliveries(1)
                 .useExponentialBackOff()
-                .retryAttemptedLogLevel(LoggingLevel.WARN)
-                .retriesExhaustedLogLevel(LoggingLevel.ERROR)
+                .retryAttemptedLogLevel(LoggingLevel.INFO)
+                .retriesExhaustedLogLevel(LoggingLevel.WARN)
                 .logExhaustedMessageHistory(true)
                 .onPrepareFailure(exchange -> {
                     Exception cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
@@ -65,36 +64,43 @@ public class PostgresLogConsumer extends RouteBuilder {
         );
 
         //TODO: should narrow exception class to keep this specific to postgres?
+        //TODO: test that this log message formatting works as expected
         // Invalid logical message format
         onException(InvalidIndividualPostgresLog.class)
                 .handled(true)
                 .useOriginalMessage()
-                .log("Routing invalid message to invalid-messages channel: ${exception.message}, invalid filename: ${headers['CamelFileName']}") //TODO: add invalid file name here
-                //TODO: encode/marshall as .gz file here
+                .log("Routing invalid message to invalid-messages channel: ${exception.message}, invalid filename: ${headers['CamelFileName']}")
                 .to(invalidMessageUri);
 
         //from("quartz://myGroup/myTestTimer?cron=*/10+*+*+*+*+?")
 
         from(consumerUri)
                 .routeId(POSTGRES_LOG_CONSUMER_ID)
-                // TODO: only unmarshall if file is encoded
-                .doTry()
-                    .unmarshal().gzipDeflater()
-                .endDoTry()
-                .doCatch(IOException.class)
-                    .log("Routing non-gzip or unreadable gzip input to invalid-messages channel: ${exception.message}, invalid filename: ${headers['CamelFileName']}")
-                    .to(invalidMessageUri)
-                    .stop()
+                .process(exchange -> {
+                    // If the file comes from a bucket instead of local storage, still populate the filename
+                    if (exchange.getIn().getHeader("CamelFileName", String.class) == null) {
+                        exchange.getIn().setHeader("CamelFileName", exchange.getIn().getHeader("CamelGoogleCloudStorageObjectName", String.class));
+                    }
+                })
+                .choice()
+                .when(header("CamelFileName").endsWith(".gz"))
+                    .doTry()
+                        .unmarshal().gzipDeflater()
+                    .endDoTry()
+                    .doCatch(IOException.class)
+                        .log("Routing non-gzip or unreadable gzip input to invalid-messages channel: ${exception.message}, invalid filename: ${headers['CamelFileName']}")
+                        .to(invalidMessageUri)
+                        .stop()
+                    .end()
                 .end()
                 .split(body().tokenize("^\\<|\n\\<")).streaming()
-                //TODO: only remove the .gz ending if it's present, only add the UUID if the .gz ending was present
                 .process(exchange -> {
-                    // TODO: added this for debugging, remove
-                    log.info("Message headers: {}", exchange.getIn().getHeaders());
-
                     String originalFileName = exchange.getIn().getHeader("CamelFileName", String.class);
-                    String newFileName = UUID.randomUUID() + "." + originalFileName.substring(0, originalFileName.length() - 3);
-                    exchange.getIn().setHeader("CamelFileName", newFileName);
+                    // If a line was part of a compressed file, strip compression extension and make unique with a UUID
+                    if (originalFileName.endsWith(".gz")) {
+                        String newFileName = UUID.randomUUID() + "." + originalFileName.substring(0, originalFileName.length() - 3);
+                        exchange.getIn().setHeader("CamelFileName", newFileName);
+                    }
                 })
                 .to(POSTGRES_LOG_ENRICH_ROUTE);
     }
