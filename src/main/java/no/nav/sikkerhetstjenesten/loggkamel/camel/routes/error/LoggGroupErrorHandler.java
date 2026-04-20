18db01573b408e3c1c48dd893f0f86a30b5cd426
Carlos Sierra <carlos.sierra@nav.no>
2026-04-20 13:30:46 +0200

    Set up teknologi-specific backout queues, with a generic fallback queue when teknologi is not available in header

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/error/LoggGroupErrorHandler.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/error/LoggGroupErrorHandler.java
@@ -3,48 +3,55 @@ package no.nav.sikkerhetstjenesten.loggkamel.camel.routes.error;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.sikkerhetstjenesten.loggkamel.camel.exceptions.dependency.DependencyException;
 import no.nav.sikkerhetstjenesten.loggkamel.camel.exceptions.invalid.InvalidLogException;
-import no.nav.sikkerhetstjenesten.loggkamel.camel.exceptions.teknologi.PostgresException;
-import org.apache.camel.Exchange;
+import no.nav.sikkerhetstjenesten.loggkamel.persistence.TeknologiEnum;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import static no.nav.sikkerhetstjenesten.loggkamel.camel.processor.enrichment.AuditloggLineMessageHeader.TEKNOLOGI;
+
 public abstract class LoggGroupErrorHandler extends RouteBuilder {
 
     @Value("${routing.postgres.dead-letter}")
-    protected String deadLetterUri;
+    protected String postgresDeadLetterUri;
 
     @Value("${routing.postgres.invalid-message}")
-    protected String invalidMessageUri;
+    protected String postgresInvalidMessageUri;
+
+    @Value("${routing.fallback.invalid-message}")
+    protected String fallbackInvalidMessageUri;
 
     @Autowired
     protected ObjectMapper objectMapper;
 
     public abstract void configure();
 
-    //TODO: currently this sends all errors to the postgres-specific channels, want technologi-specific channels instead
     public void errorHandling() {
-        onException(DependencyException.class)
+        onException(DependencyException.class).onWhen(variable(TEKNOLOGI).convertTo(TeknologiEnum.class).isEqualTo(TeknologiEnum.POSTGRESQL))
                 .useOriginalMessage()
                 .maximumRedeliveries(3)
                 .redeliveryDelay(10000) //10-second delay between retries
                 .handled(true)
-                .log("Routing DependencyException to dead-letter after retries: ${exception.message}, filename: ${headers['CamelFileName']}")
-                .to(deadLetterUri);
+                .log("Routing DependencyException to postgres dead-letter after retries: ${exception.message}, filename: ${headers['CamelFileName']}")
+                .to(postgresDeadLetterUri);
 
-        onException(InvalidLogException.class)
+        // Other teknologi-specific dead letter queues go here
+
+        onException(InvalidLogException.class).onWhen(variable(TEKNOLOGI).convertTo(TeknologiEnum.class).isEqualTo(TeknologiEnum.POSTGRESQL))
                 .useOriginalMessage()
                 .maximumRedeliveries(0)
                 .handled(true)
-                .log("Routing InvalidLogException to invalid-messages channel: ${exception.message}, filename: ${headers['CamelFileName']}")
-                .to(invalidMessageUri);
+                .log("Routing InvalidLogException to postgres invalid-messages channel: ${exception.message}, filename: ${headers['CamelFileName']}")
+                .to(postgresInvalidMessageUri);
+
+        // Other teknologi-specific invalid message queues go here
 
         onException(Exception.class)
                 .useOriginalMessage()
                 .maximumRedeliveries(0)
                 .handled(true)
-                .log(LoggingLevel.WARN, "Routing unhandled exception directly to invalid-messages channel: ${exception.class} - ${exception.message}, filename: ${headers['CamelFileName']}")
-                .to(invalidMessageUri);
+                .log(LoggingLevel.WARN, "Routing unhandled exception to fallback invalid-messages channel: ${exception.class} - ${exception.message}, filename: ${headers['CamelFileName']}")
+                .to(fallbackInvalidMessageUri);
     }
 }
