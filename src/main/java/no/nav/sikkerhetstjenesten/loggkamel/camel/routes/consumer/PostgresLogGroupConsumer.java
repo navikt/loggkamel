fb993ce1c171e550fad508dc1bfefbc0deb0797f
Carlos Sierra <carlos.sierra@nav.no>
2026-05-04 15:57:41 +0200

    Remove forcing whole file in-memory, didn't work

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/consumer/PostgresLogGroupConsumer.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/consumer/PostgresLogGroupConsumer.java
@@ -51,13 +51,6 @@ public class PostgresLogGroupConsumer extends LoggGroupErrorHandler {
                     // if log file is compressed, decompress and remove the compression extension from the filename
                     .doTry()
                         .unmarshal().gzipDeflater()
-                        //TODO: confirm this works with repeatedly backed out messages, either remove comment or remove code if does not fix issue
-                        .process(exchange -> {
-                            // Force body materialization to byte array to ensure the decompressed stream is fully consumed
-                            // and captured in-memory, preventing issues with stream references becoming stale in error handling
-                            byte[] body = exchange.getIn().getBody(byte[].class);
-                            exchange.getIn().setBody(body);
-                        })
                         .endDoTry()
                     .doCatch(Exception.class)
                         .process(exchange -> {
