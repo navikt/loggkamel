78622e9b8426f7bcdf11c72c628cb0059ef7d7ad
Carlos Sierra <carlos.sierra@nav.no>
2026-05-28 14:52:58 +0200

    Remove accidental caffeine cache from adapter

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/client/EntraProxyAdapterImpl.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/client/EntraProxyAdapterImpl.java
@@ -1,8 +1,6 @@
 package no.nav.sikkerhetstjenesten.loggkamel.client;
 
 import no.nav.sikkerhetstjenesten.loggkamel.camel.exceptions.dependency.DependencyException;
-import no.nav.sikkerhetstjenesten.loggkamel.config.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestClientResponseException;
 
@@ -14,7 +12,6 @@ public class EntraProxyAdapterImpl implements EntraProxyAdapter {
         this.client = client;
     }
 
-    @Cacheable(cacheNames = CacheConfig.ENTRA_PROXY_BY_NAV_IDENT, key = "#navIdent", sync = true)
     public EntraProxyAnsatt getAnsattFraNavIdent(String navIdent) {
         try {
             return client.getAnsattFraNavIdent(navIdent);
@@ -27,7 +24,6 @@ public class EntraProxyAdapterImpl implements EntraProxyAdapter {
         }
     }
 
-    @Cacheable(cacheNames = CacheConfig.ENTRA_PROXY_BY_T_IDENT, key = "#tIdent", sync = true)
     public EntraProxyAnsatt getAnsattFraTIdent(String tIdent) {
         try {
             return client.getAnsattFraTIdent(tIdent);
