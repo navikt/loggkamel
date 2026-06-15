6c8723da2abf2c70960a8a7f5c1f720950fa0cb4
Carlos Sierra <carlos.sierra@nav.no>
2026-06-15 10:19:10 +0200

    Move business logic out of GCPArkivLoggProducer

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/producer/GCPArkivLoggProducer.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/producer/GCPArkivLoggProducer.java
@@ -1,30 +1,12 @@
 package no.nav.sikkerhetstjenesten.loggkamel.camel.routes.producer;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.google.cloud.logging.LogEntry;
-import com.google.cloud.logging.Logging;
-import com.google.cloud.logging.LoggingOptions;
-import com.google.cloud.logging.Payload;
-import com.google.cloud.logging.Severity;
 import no.nav.boot.conditionals.ConditionalOnGCP;
-import no.nav.sikkerhetstjenesten.loggkamel.camel.exceptions.dependency.GCPDependencyException;
-import no.nav.sikkerhetstjenesten.loggkamel.camel.processor.enrichment.EnrichedAuditlogg;
-import no.nav.sikkerhetstjenesten.loggkamel.observability.Metrics;
-import no.nav.sikkerhetstjenesten.loggkamel.rest.dto.AuditloggArkivResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import no.nav.sikkerhetstjenesten.loggkamel.camel.processor.producer.GCPArkivLoggProducerProcessor;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.Map;
-
-import static no.nav.sikkerhetstjenesten.loggkamel.camel.processor.enrichment.AuditloggLineMessageHeader.*;
 
 @Component
 @ConditionalOnGCP
 public class GCPArkivLoggProducer extends ArkivLoggProducer {
-
-    @Autowired
-    private Metrics metrics;
 
     @Override
     public void configure() {
@@ -33,33 +15,7 @@ public class GCPArkivLoggProducer extends ArkivLoggProducer {
         from(ARKIVLOGG_PRODUCER_ROUTE)
             .routeId(ARKIVLOGG_PRODUCER_ID)
             .log("Producing log message ${header.CamelFileName} to GCP Logging")
-            .process(exchange -> {
-                metrics.incrementHappyPath(Metrics.Multiplicity.single,  exchange.getVariable(TEKNOLOGI, String.class).toLowerCase(), Metrics.Action.produced);
-                String dbName = exchange.getVariable(AUDITLOGG_ARKIV, AuditloggArkivResponseDTO.class).getDbname();
-                metrics.incrementDatabaseSpecificAction(dbName,  exchange.getVariable(TEKNOLOGI, String.class).toLowerCase(), Metrics.Action.produced);
-            })
-            .process(exchange -> {
-                String targetGCPProjectId = exchange.getVariable(TEAM_GCP_PROJECT_ID, String.class);
-
-                try (Logging logging = LoggingOptions.newBuilder()
-                        .setProjectId(targetGCPProjectId)
-                        .build()
-                        .getService()) {
-
-                    Map<String, Object> logMessageAsMap = objectMapper.convertValue(exchange.getMessage().getBody(EnrichedAuditlogg.class), new TypeReference<>() {});
-                    Payload.JsonPayload logMessageAsJsonPayload = Payload.JsonPayload.of(logMessageAsMap);
-
-                    LogEntry entry =
-                            LogEntry.newBuilder(logMessageAsJsonPayload)
-                                    .setSeverity(Severity.INFO)
-                                    .setLogName("loggkamel-arkiv")
-                                    .build();
-
-                    logging.write(Collections.singleton(entry));
-                } catch (Exception e) {
-                    log.warn("Error while writing log entry to GCP Logging for file {}, error message: {}", exchange.getMessage().getHeader("CamelFileName", String.class), e.getMessage());
-                    throw new GCPDependencyException("Error while writing log entry to GCP Logging for file " + exchange.getMessage().getHeader("CamelFileName", String.class), e);
-                }
-            });
+            .bean(GCPArkivLoggProducerProcessor.class, "incrementMetrics")
+            .bean(GCPArkivLoggProducerProcessor.class, "writeToGcpLogging");
     }
 }
