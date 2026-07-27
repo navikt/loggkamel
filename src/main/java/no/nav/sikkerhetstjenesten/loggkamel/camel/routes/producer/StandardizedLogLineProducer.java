3afb1e7e03073d06bb805f8e86efe4ff623508cc
Carlos Sierra <carlos.sierra@nav.no>
2026-07-27 10:25:52 +0200

    Remove blocking on disabled route

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/producer/StandardizedLogLineProducer.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/camel/routes/producer/StandardizedLogLineProducer.java
@@ -5,5 +5,5 @@ import no.nav.sikkerhetstjenesten.loggkamel.camel.routes.error.LogPacketErrorHan
 public abstract class StandardizedLogLineProducer extends LogPacketErrorHandler {
 
     public static final String STANDARDIZED_LOG_LINE_PRODUCER_ID = "standardized-log-line-producer";
-    public static final String STANDARDIZED_LOG_LINE_PRODUCER_ROUTE = "direct:" + STANDARDIZED_LOG_LINE_PRODUCER_ID + "?failIfNoConsumers=false&block=true&timeout=61000";
+    public static final String STANDARDIZED_LOG_LINE_PRODUCER_ROUTE = "direct:" + STANDARDIZED_LOG_LINE_PRODUCER_ID + "?failIfNoConsumers=false";
 }
