3dce5b22fd7c21aae117f167b0032b110683fcd8
Carlos Sierra <carlos.sierra@nav.no>
2026-08-03 08:04:35 +0200

    Allow log discarding only if no specific log types are being forwarded

--- src/main/resources/db/migration/V13__add_discard_logs_field_and_let_naisteam_be_null.sql
+++ src/main/resources/db/migration/V13__add_discard_logs_field_and_let_naisteam_be_null.sql
@@ -1,4 +1,6 @@
 ALTER TABLE oversikt
     ADD COLUMN IF NOT EXISTS discard_logs BOOLEAN DEFAULT false,
     ALTER COLUMN naisteam DROP NOT NULL,
-    ADD CONSTRAINT naisteam_or_discard CHECK (naisteam IS NOT NULL or discard_logs IS true);
+    ADD CONSTRAINT naisteam_or_discard CHECK (naisteam IS NOT NULL or discard_logs IS true),
+    ADD CONSTRAINT no_forwarding_types_on_discard CHECK (discard_logs is false or (okonomi is false and arkivlov is false and logging_leseoperasjoner is false))
+;
