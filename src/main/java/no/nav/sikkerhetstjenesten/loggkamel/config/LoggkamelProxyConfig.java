bc6b249623e6b4af3b7c5cd0410c6010b03e0391
Carlos Sierra <carlos.sierra@nav.no>
2026-08-05 09:17:04 +0200

    Configure external loggkamel-proxy endpoint, firewall timeouts

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/config/LoggkamelProxyConfig.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/config/LoggkamelProxyConfig.java
@@ -1,15 +1,26 @@
 package no.nav.sikkerhetstjenesten.loggkamel.config;
 
+import no.nav.boot.conditionals.ConditionalOnGCP;
 import no.nav.sikkerhetstjenesten.loggkamel.auth.LoggkamelProxyAuthInterceptor;
 import no.nav.sikkerhetstjenesten.loggkamel.client.LoggkamelProxyClient;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
+@ConditionalOnGCP
 public class LoggkamelProxyConfig {
 
     @Value("${LOGGKAMEL_PROXY_BASE_URL}")
@@ -17,8 +28,24 @@ public class LoggkamelProxyConfig {
 
     @Bean
     public LoggkamelProxyClient loggkamelProxyClient(LoggkamelProxyAuthInterceptor loggkamelProxyAuthInterceptor) {
+
+        ConnectionConfig connectionConfig = ConnectionConfig.custom()
+                .setIdleTimeout(Timeout.ofMinutes(55))
+                .setTimeToLive(59, TimeUnit.MINUTES)
+                .build();
+
+        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+                .setDefaultConnectionConfig(connectionConfig)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(cm)
+                .evictExpiredConnections()
+                .build();
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(loggkamelProxyBaseUrl)
+                .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
                 .requestInterceptors(interceptors -> {
                     interceptors.add(new LoggingRequestInterceptor());
                     interceptors.add(loggkamelProxyAuthInterceptor);
