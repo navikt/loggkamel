7ca8fffca2b473e41276271a380d0a29ce4084bc
Carlos Sierra <carlos.sierra@nav.no>
2026-09-14 08:37:53 +0200

    Add /v3/api-docs to permitted endpoints

--- src/main/java/no/nav/sikkerhetstjenesten/loggkamel/config/SpringSecurityConfig.java
+++ src/main/java/no/nav/sikkerhetstjenesten/loggkamel/config/SpringSecurityConfig.java
@@ -27,6 +27,7 @@ public class SpringSecurityConfig {
                             authorize.requestMatchers("/monitoring/**").permitAll();
                             authorize.requestMatchers("/api/v1/dev/**").permitAll();
                             authorize.requestMatchers("/swagger-ui/**").permitAll();
+                            authorize.requestMatchers("/v3/api-docs/**").permitAll();
                             authorize.requestMatchers("/api/v1/naisteam/futureNaisteamListEndpoint").authenticated();
                             authorize.requestMatchers("/api/v1/naisteam/**").permitAll();
                             authorize.anyRequest().authenticated();
